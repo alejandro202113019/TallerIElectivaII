13e954ab794d0fda4d6093e3999cc19205237516
Camilo Correa <cristian.correa01@uptc.edu.co>
2024-10-12 17:42:05 -0500

    Ajuste para desplegue en render libreria bcrytp

--- controllers/controll-Auth.js
+++ controllers/controll-Auth.js
@@ -1,6 +1,6 @@
 //controllers/controll-Auth.js
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES = process.env.JWT_EXPIRES || '1h';
